46a199def85be257a22b8a0fc434a6ad3a7b50b9
Bastab Barik <135296571+BastabBarik-01@users.noreply.github.com>
2024-08-18 18:28:46 +0530

    Update preprocessor.py

--- whatsapp-chat-analysis-main/preprocessor.py
+++ whatsapp-chat-analysis-main/preprocessor.py
@@ -2,13 +2,14 @@ import re
 import pandas as pd
 
 def preprocess(data):
-    pattern = '\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s'
+    # Use a raw string to avoid issues with backslashes
+    pattern = r'\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s'
 
     messages = re.split(pattern, data)[1:]
     dates = re.findall(pattern, data)
 
     df = pd.DataFrame({'user_message': messages, 'message_date': dates})
-    # convert message_date type
+    # Convert message_date type
     df['message_date'] = pd.to_datetime(df['message_date'], format='%d/%m/%Y, %H:%M - ')
 
     df.rename(columns={'message_date': 'date'}, inplace=True)
@@ -16,7 +17,8 @@ def preprocess(data):
     users = []
     messages = []
     for message in df['user_message']:
-        entry = re.split('([\w\W]+?):\s', message)
+        # Use a raw string to avoid issues with backslashes
+        entry = re.split(r'([\w\W]+?):\s', message)
         if entry[1:]:  # user name
             users.append(entry[1])
             messages.append(" ".join(entry[2:]))
